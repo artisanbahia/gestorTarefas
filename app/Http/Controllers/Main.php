98857d4a367011121999e023821aa79d4517b1c2
Luis Cerqueira <dev.luiscerqueira@gmail.com>
2024-04-22 11:38:34 -0300

    Depois da criação dos MiddleWare de Login e Logout clear

--- app/Http/Controllers/Main.php
+++ app/Http/Controllers/Main.php
@@ -7,29 +7,62 @@ use Illuminate\Support\Facades\DB;
 
 class Main extends Controller
 {
-    
-    public function index(){
-        
-        echo "Gestor de tarefas";
 
+    public function index()
+    {
+        echo "Gestor de tarefas";
     }
 
-    public function login(){
 
+
+// -----------------------------------------------------------------------------------
+// Método de login
+//------------------------------------------------------------------------------------
+
+    public function login()
+    {
         $data = [
             'title' => 'Login'
         ];
-
         return view('login_frm', $data);
     }
 
-    public function login_frm(){
-        echo "login_frm";
+
+    public function login_submit()
+    {
+        echo "Logado";
+    }
+
+    public function login_frm()
+    {
+        session()->put('username', 'admin'); // Maneira de setar (colocar) uma variável
+        echo "usuário Logado";
+    }
+
+
+    public function logout()
+    {
+        session()->forget('username'); // Esqueça a variável username
+        return redirect()->route('login'); // Redirecione para  página de login
+    }
+
+// -------------------------------------------------------------------------------------
+// Método de acesso pós logoinz
+//--------------------------------------------------------------------------------------
+
+
+    public function main()
+    {
+        $data = [
+            'title' => 'Gestor de Tarefas'
+        ];
+
+        return view('main', $data);
     }
 
 
 
-    
+
 }
 
 
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Main;
+use App\Http\Middleware\CheckLogin; // Trazendo o middleware para uso conjunto com as rotas de login
+use App\Http\Middleware\CheckLogout;
 
 // -----> Modelos de rota
 
@@ -12,17 +14,30 @@ use App\Http\Controllers\Main;
 // Route::get('/', [Main::class, 'index']);
 
 
+
+
+
 //----------------------------------------------------------------------------------------------
 
 
-// Entrada da aplicação
-Route::get('/', [Main::class, 'index'])->name('index'); // atribuindo um nome para esta rota
 
 
 // Rota para formulário de Login
 
-Route::get('/login', [Main::class, 'login'])->name('login');
-Route::post('/login_submit', [Main::class, 'login_submit'])->name('login_submit'); // Método post
+
+// OUT APP -
+
+    Route::get('/login', [Main::class, 'login'])->name('login')->middleware(CheckLogout::class); // Nova maneira de referenciar um Middleware no Laravel 11, que não traz o script de Kernel na pasta HTTP
+    Route::post('/login_submit', [Main::class, 'login_submit'])->name('login_submit'); // Método post
+
+
+
+// IN APP - Route witch Middleware [Rotas abrangidas pela verificação]
+
+    Route::get('/main', [Main::class, 'main'])->name('main')->middleware(CheckLogin::class); // O acesso à rota Main requer verificação da regra do Middleware. Caso 'caia na malha fina' do Middleware, executa a regra do Middleware. Do contrário, executa o método que está previsto no controller.
+    Route::get('/', [Main::class, 'main'])->name('index')->middleware(CheckLogin::class);
+    Route::get('/logout', [Main::class, 'logout'])->name('logout')->middleware(CheckLogin::class); // Se tentar fazer logout sem estar logado, executa o CheckLogin
+
 
 
 
